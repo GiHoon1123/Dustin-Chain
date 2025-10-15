58a641a2ca3ab95deee9123c83a406c8cbfd7673
GiWoonHwang <85157729+GiWoonHwang@users.noreply.github.com>
2025-10-15 21:57:01 +0900

    feat: StateModule을 글로벌 모듈로 설정
    - StateModule에 @Global() 데코레이터 추가
    - StateManager를 전역으로 제공하여 모든 모듈에서 자동 주입 가능
    - CryptoService 중복 provider 제거 (CommonModule에서 이미 글로벌 제공)
    - AppModule에 StateModule import 추가
    - 이더리움 2.0 아키텍처에 맞는 전역 상태 관리 구조 구현
    
    장점:
    - 의존성 주입 간소화
    - 중복 코드 제거
    - 상태 관리 서비스 전역 접근성 향상

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AccountModule } from './account/account.module';
 import { BlockModule } from './block/block.module';
 import { CommonModule } from './common/common.module';
 import { ConsensusModule } from './consensus/consensus.module';
+import { StateModule } from './state/state.module';
 import { TransactionModule } from './transaction/transaction.module';
 import { ValidatorModule } from './validator/validator.module';
 
@@ -11,8 +12,11 @@ import { ValidatorModule } from './validator/validator.module';
  *
  * 애플리케이션의 루트 모듈
  *
- * Modules:
+ * Global Modules:
  * - CommonModule: 전역 유틸리티 (CryptoService 등)
+ * - StateModule: 전역 상태 관리 (StateManager)
+ *
+ * Feature Modules:
  * - AccountModule: 계정 상태 관리
  * - TransactionModule: 트랜잭션 관리
  * - ValidatorModule: Validator 관리 (Proposer/Committee 선택)
@@ -21,7 +25,8 @@ import { ValidatorModule } from './validator/validator.module';
  */
 @Module({
   imports: [
-    CommonModule,
+    CommonModule, // @Global() - CryptoService 전역 제공
+    StateModule, // @Global() - StateManager 전역 제공
     AccountModule,
     TransactionModule,
     ValidatorModule,
--- src/state/state.module.ts
+++ src/state/state.module.ts
@@ -1,6 +1,27 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { StateManager } from './state-manager';
 
+/**
+ * StateModule
+ *
+ * 전역 모듈로 선언하여 모든 모듈에서 StateManager 자동 사용 가능
+ *
+ * 이더리움 2.0에서의 패턴:
+ * - StateManager는 모든 모듈에서 필요 (Account, Transaction, Block, Consensus 등)
+ * - 상태 관리는 블록체인의 핵심이므로 전역으로 제공하는 것이 효율적
+ *
+ * 왜 @Global() 사용:
+ * - StateManager는 거의 모든 모듈에서 사용
+ * - 매번 import 하는 것보다 전역으로 제공하는 것이 효율적
+ * - CryptoService는 이미 CommonModule에서 글로벌로 제공되므로 중복 제거
+ *
+ * 포함된 서비스:
+ * - StateManager: 상태 관리, 캐시, 저널링, LevelDB
+ *
+ * 의존성:
+ * - CryptoService: CommonModule에서 글로벌로 제공
+ */
+@Global()
 @Module({
   providers: [StateManager],
   exports: [StateManager],
